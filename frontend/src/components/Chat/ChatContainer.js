acd51e0cbb3cb93f2976b4e33e9371be7232d544
Kruthik T shekhar <99882108+Kruthiktshekar@users.noreply.github.com>
2024-11-26 14:31:20 +0530

    Update ChatContainer.js

--- frontend/src/components/Chat/ChatContainer.js
+++ frontend/src/components/Chat/ChatContainer.js
@@ -8,7 +8,7 @@ function ChatContainer() {
   
   const [currentChat, setCurrentChat] = useState({})
 
-  const socket = io('https://mini-blog-app-api.onrender.com:3232')
+  const socket = io('https://mini-blog-app-api.onrender.com')
 
   return (
     <div>
